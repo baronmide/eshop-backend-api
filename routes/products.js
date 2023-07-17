a195c07d56fe3d63c1bc1e21d888c0534260c44e
baronmide <danieladegbite@gmail.com>
2023-07-17 10:13:40 +0300

    products and categories setup

--- routes/products.js
+++ routes/products.js
@@ -2,21 +2,31 @@ const {Product} = require('../models/product');
 const express = require('express');
 const { Category } = require('../models/category');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+
 
 router.get('/', async (req,res)=>{
-    const productList = await Product.find();
+    // localhost:3150/api/v1/products?categories=232344, 234455
+    let filter = {};
+    if(req.query.categroies)
+    {
+        filter = {category: req.query.categories.split(',')}
+    }
+    
+    const productList = await Product.find(filter).populate('category');
 
     if(!productList) {
-        res.status(500).json({success: false})
+       return res.status(500).json({success: false})
     }
     res.send(productList)
 })
 
 router.get('/:id', async (req,res)=>{
-    const product = await Product.findById(req.params.id);
+    const product = await Product.findById(req.params.id).populate('category');
 
     if(!product) {
-        res.status(500).json({success: false})
+       return res.status(500).json({success: false})
     }
     res.send(product);
 })
@@ -47,5 +57,60 @@ router.post('/', async (req,res)=>{
 
     res.send(product)
 })
+
+
+router.put('/:id', async (req, res)=> {
+    if(!mongoose.isValidObjectId(req.params.id)) {
+        res.status(400).send('Invalid Product Id')
+    }
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('Invalid Category')
+
+const product = await Product.findByIdAndUpdate(
+    req.params.id,
+    {
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image: req.body.image,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured
+    },
+    { new: true }
+)
+
+if(!product)
+return res.status(400).send('the product cannot be updated')
+
+res.send(product);
+})
+
+router.delete('/:id', (req, res) => {
+    Product.findByIdAndDelete(req.params.id).then(product=>{
+        if(product) {
+            return res.status(200).json({success: true, message: 'the product is deleted!'})
+        } else {
+            return res.status.apply(404).json({success: false, message: 'product not found!'})
+        }
+        }).catch(err=>{
+            return res.status(400).json({success: false, error: err})
+        })
+})
+
+router.get('/get/featured/:count', async (req,res)=>{
+    const count = req.params.count ? req.params.count : 0
+    const products = await Product.find({isFeatured: true}).limit(+count)
+
+    if(!products) {
+       return res.status(500).json({success: false})
+    }
+    res.send(products);
+})
+
 
 module.exports = router;
